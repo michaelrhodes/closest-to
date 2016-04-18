5ecad15978b3a20701c0e168d85d601b3c8ecfd5
michaelrhodes <spam@michaelrhod.es>
2016-04-18 13:55:12 -0700

    Make arbitrary style changes.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-var total = function(target) {
-  if (!Array.isArray(target)) {
-    return target
-  }
-  return target.reduce(function(a, b) {
-    return a + b
-  })
-}
+var abs = Math.abs
 
-module.exports = function(target, numbers) {
+function closest (target, numbers) {
   var closest = Infinity
   var difference = 0
   var winner = null
 
-  numbers.sort(function(a, b) {
+  numbers.sort(function (a, b) {
     return total(a) - total(b)
   })
 
   for (var i = 0, l = numbers.length; i < l; i++) {  
-    difference = Math.abs(total(target) - total(numbers[i]))
-    if (difference >= closest) {
-      break
-    }
+    difference = abs(total(target) - total(numbers[i]))
+    if (difference >= closest) break
     closest = difference
     winner = numbers[i]
   }
 
   return winner
 }
+
+function total (target) {
+  if (!Array.isArray(target)) {
+    return target
+  }
+  return target.reduce(function(a, b) {
+    return a + b
+  }, 0)
+}
+
+module.exports = closest
